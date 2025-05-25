347f2539126733d1f7f5d01440eafc6ba4f08f90
Sudharaka <sudharakap@mun.ca>
2025-05-25 16:27:45 -0700

    feat: initial checkin with package installations

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,20 +3,29 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `python-sinhala`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Python Sinhala`,
+    author: `OpenArchitex`,
+    description: `Website for Python Sinhala`,
+    siteUrl: `https://www.pythonsinhala.com`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/favicon.svg",
+      },
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
 };
